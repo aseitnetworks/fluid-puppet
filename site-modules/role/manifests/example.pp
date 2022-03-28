9dc59015e4b41bb8d91a9bea80266322dd14f4bd
Daniel Ward <735837+DanWard@users.noreply.github.com>
2022-03-28 11:22:28 +1100

    fix file content

--- site-modules/role/manifests/example.pp
+++ site-modules/role/manifests/example.pp
@@ -1,6 +1,6 @@
 class role::example {     
   $testVariable = 'I am variable from init.pp'         
   file { '/tmp/testModule': 
-    content => template("hellotemplate/testTemplate.erb"),         
+    content => $testVariable,         
   } 
 }
