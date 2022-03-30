edd8ec21ecdf5e2c61822709855f204ea6a83230
Daniel Ward <735837+DanWard@users.noreply.github.com>
2022-03-30 12:51:30 +1100

    split files

--- site-modules/role/manifests/example.pp
+++ site-modules/role/manifests/example.pp
@@ -1,18 +1,3 @@
-class role::kernel {     
-  file { '/etc/sysctl.d/90-kubelet.conf':
-    ensure   => file,
-    source   => 'puppet:///modules/role/90-kubelet.conf',
-    mode     => '0644',
-    owner    => 'root',
-    group    => 'root',
-    before => Exec['Set kernel params'],
-  }
-  exec { 'Set kernel params':
-    command  => 'sysctl -p /etc/sysctl.d/90-kubelet.conf',
-    user     => 'root',
-  }
-}
-
 class role::example {     
   $nodeType = 'switchmaster'   
 
---
+++ site-modules/role/manifests/kernel.pp
@@ -0,0 +1,14 @@
+class role::kernel {     
+  file { '/etc/sysctl.d/90-kubelet.conf':
+    ensure   => file,
+    source   => 'puppet:///modules/role/90-kubelet.conf',
+    mode     => '0644',
+    owner    => 'root',
+    group    => 'root',
+    before => Exec['Set kernel params'],
+  }
+  exec { 'Set kernel params':
+    command  => 'sysctl -p /etc/sysctl.d/90-kubelet.conf',
+    user     => 'root',
+  }
+}
