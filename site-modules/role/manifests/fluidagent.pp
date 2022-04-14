f29612a5db7017b42ba1182c275c976a5e9f1e14
Daniel Ward <735837+DanWard@users.noreply.github.com>
2022-04-14 17:52:01 +1000

    fix: release type

--- site-modules/role/manifests/fluidagent.pp
+++ site-modules/role/manifests/fluidagent.pp
@@ -3,7 +3,7 @@ class role::fluidagent {
   
   file { '/etc/apt/sources.list.d/internal.list':
     ensure   => file,
-    content  => 'deb https://apt.fluid.aseit.com.au/ $releaseType main',
+    content  => 'deb https://apt.fluid.aseit.com.au/ ${releaseType} main',
     mode     => '0644',
     owner    => 'root',
     group    => 'root',
