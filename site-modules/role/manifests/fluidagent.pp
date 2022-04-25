ebe04e428fb6ba8a54684aa197f9607d8c3332b5
Daniel Ward <735837+DanWard@users.noreply.github.com>
2022-04-26 09:32:05 +1000

    feat: add apt key

--- site-modules/role/manifests/fluidagent.pp
+++ site-modules/role/manifests/fluidagent.pp
@@ -6,6 +6,11 @@ class role::fluidagent {
     owner    => 'root',
     group    => 'root',
   }
+  -> exec { 'Install fluid gpt key':
+    command  => '/usr/bin/sh -c "wget -qO - https://apt.fluid.aseit.com.au/public.key | apt-key add -"',
+    user     => 'root',
+    unless  => "/usr/bin/test -e /usr/bin/fluid-agent",
+  }
   -> exec { 'Install agent':
     command  => '/usr/bin/sh -c "apt-get update && apt-get install -y fluid-agent"',
     user     => 'root',
