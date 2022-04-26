466499bc8a2fd4d3b549c1f5e60f4280aaf6e572
Daniel Ward <735837+DanWard@users.noreply.github.com>
2022-04-26 18:01:14 +1000

    fix: node selection

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,16 +3,18 @@
 File { backup => false }
 
 node default {
-  include role::kernel
-  include role::multipath
 }
 
 node /^fluid-master.*$/ {
+  include role::kernel
+  include role::multipath
   include role::fluidagent
   include role::k3smaster
 }
 
 node /^fluid-agent.*$/ {
+  include role::kernel
+  include role::multipath
   include role::fluidagent
   include role::k3sagent
 }
